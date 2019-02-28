f25e08cbd9cd67f5be8647b3a17d8a66f2056d5a
Touch-Me <adolf.hazmi@yahoo.com>
2019-02-28 17:58:15 +0100

    Setting up the Toolbar with the sideDrawer (finally)

--- app/src/main/java/com/example/touch_me/pfe_project/WelcomeScreen.java
+++ app/src/main/java/com/example/touch_me/pfe_project/WelcomeScreen.java
@@ -1,40 +1,33 @@
 package com.example.touch_me.pfe_project;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.ViewPropertyAnimatorCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import io.codetail.animation.ViewAnimationUtils;
-import yalantis.com.sidemenu.interfaces.Resourceble;
-import yalantis.com.sidemenu.interfaces.ScreenShotable;
-import yalantis.com.sidemenu.model.SlideMenuItem;
-import yalantis.com.sidemenu.util.ViewAnimator;
-
-
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
-import android.transition.Fade;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import yalantis.com.sidemenu.interfaces.Resourceble;
+import yalantis.com.sidemenu.interfaces.ScreenShotable;
+import yalantis.com.sidemenu.model.SlideMenuItem;
+import yalantis.com.sidemenu.util.ViewAnimator;
 
 
 public class WelcomeScreen extends AppCompatActivity implements yalantis.com.sidemenu.util.ViewAnimator.ViewAnimatorListener {
@@ -45,21 +38,29 @@ public class WelcomeScreen extends AppCompatActivity implements yalantis.com.sid
   private ContentFragment contentFragment;
   private ViewAnimator viewAnimator;
   private ActionBarDrawerToggle drawer;
+  private Button btnPullDrawer;
+  private DrawerLayout.DrawerListener dl;
+  private Toolbar toolbar;
+  private TextView toolbarTitle;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.nav_layout);
+
     setStatusBarTrasparent();
 //    setupWindowAnimations();
-    overridePendingTransition(R.anim.slide_in,R.anim.slide_out);
+    overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
     contentFragment = ContentFragment.newInstance(R.drawable.content_music);
     getSupportFragmentManager().beginTransaction()
       .replace(R.id.content_frame, contentFragment)
       .commit();
     drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
     drawerLayout.setScrimColor(Color.TRANSPARENT);
     linearLayout = (LinearLayout) findViewById(R.id.left_drawer);
-
+    toolbar = (Toolbar) findViewById(R.id.toolbar);
+    toolbarTitle = (TextView) findViewById(R.id.toolbar_title);
 
 
     linearLayout.setOnClickListener(new View.OnClickListener() {
@@ -72,30 +73,48 @@ public class WelcomeScreen extends AppCompatActivity implements yalantis.com.sid
     createMenuList();
 
     viewAnimator = new ViewAnimator<>(this, list, contentFragment, drawerLayout, this);
-    //to open menu you have to override ActionBarDrawerToggle method
+    demoHolder();
+  }
 
-
+  public void demoHolder() {
+    //make the status bar text color grey(to be visible with light background)
+    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setTitle(null);
+    toolbarTitle.setText("<<Title>>");
   }
 
   private void setActionBar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
-    getSupportActionBar().setHomeButtonEnabled(true);
-    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    drawer = new ActionBarDrawerToggle(WelcomeScreen.this, drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close ){
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//    setSupportActionBar(toolbar);
+//    getSupportActionBar().setHomeButtonEnabled(true);
+//    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    dl = new DrawerLayout.DrawerListener() {
+      //    drawer = new ActionBarDrawerToggle(WelcomeScreen.this, drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close ){
       @Override
       public void onDrawerSlide(View drawerView, float slideOffset) {
-        super.onDrawerSlide(drawerView, slideOffset);
+//        super.onDrawerSlide(drawerView, slideOffset);
         if (slideOffset > 0.6 && linearLayout.getChildCount() == 0)
           viewAnimator.showMenuContent();
       }
+
+      @Override
+      public void onDrawerOpened(@NonNull View drawerView) {
+
+      }
+
       public void onDrawerClosed(View view) {
-        super.onDrawerClosed(view);
+//        super.onDrawerClosed(view);
         linearLayout.removeAllViews();
         linearLayout.invalidate();
       }
+
+      @Override
+      public void onDrawerStateChanged(int newState) {
+
+      }
     };
-    drawerLayout.setDrawerListener(drawer);
+    drawerLayout.addDrawerListener(dl);
   }
 
   @Override
@@ -111,13 +130,14 @@ public class WelcomeScreen extends AppCompatActivity implements yalantis.com.sid
       w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
   }
+
   private void createMenuList() {
     SlideMenuItem menuItem0 = new SlideMenuItem(ContentFragment.CLOSE, R.drawable.icn_close);
     list.add(menuItem0);
-    SlideMenuItem menuItem = new SlideMenuItem(ContentFragment.BUILDING, R.drawable.icn_1);
+    SlideMenuItem menuItem = new SlideMenuItem(ContentFragment.BUILDING, R.drawable.icn_close);
     list.add(menuItem);
-//    SlideMenuItem menuItem2 = new SlideMenuItem(ContentFragment.BOOK, R.drawable.icn_2);
-//    list.add(menuItem2);
+    SlideMenuItem menuItem2 = new SlideMenuItem(ContentFragment.BOOK, R.drawable.icn_close);
+    list.add(menuItem2);
 //    SlideMenuItem menuItem3 = new SlideMenuItem(ContentFragment.PAINT, R.drawable.icn_3);
 //    list.add(menuItem3);
 //    SlideMenuItem menuItem4 = new SlideMenuItem(ContentFragment.CASE, R.drawable.icn_4);
@@ -130,42 +150,6 @@ public class WelcomeScreen extends AppCompatActivity implements yalantis.com.sid
 //    list.add(menuItem7);
   }
 
-
-
-//  private void setActionBar() {
-//    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//    setSupportActionBar(toolbar);
-//    getSupportActionBar().setHomeButtonEnabled(true);
-//    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//    drawerToggle = new ActionBarDrawerToggle(
-//      this,                  /* host Activity */
-//      drawerLayout,         /* DrawerLayout object */
-//      toolbar,  /* nav drawer icon to replace 'Up' caret */
-//      R.string.drawer_open,  /* "open drawer" description */
-//      R.string.drawer_close  /* "close drawer" description */
-//    ) {
-//
-//      /** Called when a drawer has settled in a completely closed state. */
-//      public void onDrawerClosed(View view) {
-//        super.onDrawerClosed(view);
-//        linearLayout.removeAllViews();
-//        linearLayout.invalidate();
-//      }
-//
-//      @Override
-//      public void onDrawerSlide(View drawerView, float slideOffset) {
-//        super.onDrawerSlide(drawerView, slideOffset);
-//        if (slideOffset > 0.6 && linearLayout.getChildCount() == 0)
-//          viewAnimator.showMenuContent();
-//      }
-//
-//      /** Called when a drawer has settled in a completely open state. */
-//      public void onDrawerOpened(View drawerView) {
-//        super.onDrawerOpened(drawerView);
-//      }
-//    };
-//    drawerLayout.setDrawerListener(drawerToggle);
-//  }
 
   @Override
   protected void onPostCreate(Bundle savedInstanceState) {
@@ -188,17 +172,16 @@ public class WelcomeScreen extends AppCompatActivity implements yalantis.com.sid
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
-//    if (drawerToggle.onOptionsItemSelected(item)) {
-//      return true;
-//    }
-//    switch (item.getItemId()) {
-//      case R.id.action_settings:
-//        return true;
-//      default:
-//        return super.onOptionsItemSelected(item);
-//    }
-//    else
-      return false;
+    if (super.onOptionsItemSelected(item)) {
+      return true;
+    }
+    switch (item.getItemId()) {
+      case R.id.action_settings:
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+
   }
 
   private ScreenShotable replaceFragment(ScreenShotable screenShotable, int topPosition) {
@@ -208,11 +191,11 @@ public class WelcomeScreen extends AppCompatActivity implements yalantis.com.sid
 //    SupportAnimator animator = ViewAnimationUtils.createCircularReveal(view, 0, topPosition, 0, finalRadius);
 //    animator.setInterpolator(new AccelerateInterpolator());
 //    animator.setDuration(ViewAnimator.CIRCULAR_REVEAL_ANIMATION_DURATION);
-//
-//    findViewById(R.id.content_overlay).setBackgroundDrawable(new BitmapDrawable(getResources(), screenShotable.getBitmap()));
+
+    findViewById(R.id.content_overlay).setBackgroundDrawable(new BitmapDrawable(getResources(), screenShotable.getBitmap()));
 //    animator.start();
 //    ContentFragment contentFragment = ContentFragment.newInstance(this.res);
-//    getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, contentFragment).commit();
+    getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, contentFragment).commit();
     return contentFragment;
   }
 
@@ -228,13 +211,13 @@ public class WelcomeScreen extends AppCompatActivity implements yalantis.com.sid
 
   @Override
   public void disableHomeButton() {
-    getSupportActionBar().setHomeButtonEnabled(false);
+//    getSupportActionBar().setHomeButtonEnabled(false);
 
   }
 
   @Override
   public void enableHomeButton() {
-    getSupportActionBar().setHomeButtonEnabled(true);
+//    getSupportActionBar().setHomeButtonEnabled(true);
     drawerLayout.closeDrawers();
 
   }
